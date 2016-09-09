e6cd17118fab9e351c42214d1054d765ecc04e8f
Andrew Throener <athroener@mystartupsolutions.com>
2016-09-09 11:25:07 -0500

    update

--- dist/index.js
+++ dist/index.js
@@ -15,15 +15,3 @@ Object.keys(_module).forEach(function (key) {
     }
   });
 });
-
-var _config = require('./config');
-
-Object.keys(_config).forEach(function (key) {
-  if (key === "default" || key === "__esModule") return;
-  Object.defineProperty(exports, key, {
-    enumerable: true,
-    get: function get() {
-      return _config[key];
-    }
-  });
-});
