ca19cb14f2595fba56d8c9b7fa3a4de85b56b4b2
Andrew Throener <athroener@mystartupsolutions.com>
2016-09-12 23:03:46 -0500

    Quick Release Patch

--- dist/config.js
+++ dist/config.js
@@ -3,28 +3,31 @@
 Object.defineProperty(exports, "__esModule", {
   value: true
 });
-exports.default = {
-  host: process.env.EXPRESSMODULE_HOST || '0.0.0.0',
-  http: {
-    port: process.env.EXPRESSMODULE_HTTP_PORT || '8080'
-  },
-  https: {
-    enable: process.env.EXPRESSMODULE_HTTPS || 'true', //Enabling SSL makes the entire site forced over SSL.
-    port: process.env.EXPRESSMODULE_HTTPS_PORT || '8443',
-    options: {
-      key: process.env.EXPRESSMODULE_HTTPS_KEY || __dirname + '/ssl/key.pem',
-      cert: process.env.EXPRESSMODULE_HTTPS_CERT || __dirname + '/ssl/cert.pem'
+
+exports.default = () => {
+  return {
+    host: process.env.EXPRESSMODULE_HOST || '0.0.0.0',
+    http: {
+      port: process.env.EXPRESSMODULE_HTTP_PORT || '8080'
+    },
+    https: {
+      enable: process.env.EXPRESSMODULE_HTTPS || 'true', //Enabling SSL makes the entire site forced over SSL.
+      port: process.env.EXPRESSMODULE_HTTPS_PORT || '8443',
+      options: {
+        key: process.env.EXPRESSMODULE_HTTPS_KEY || __dirname + '/ssl/key.pem',
+        cert: process.env.EXPRESSMODULE_HTTPS_CERT || __dirname + '/ssl/cert.pem'
+      }
+    },
+    logs: {
+      //https://github.com/expressjs/morgan
+      morgan: {
+        enable: process.env.EXPRESSMODULE_MORGAN_ENABLE || 'true',
+        format: process.env.EXPRESSMODULE_MORGAN_FORMAT || 'short'
+      }
+    },
+    helmet: {
+      enable: process.env.EXPRESSMODULE_HELMET_ENABLE || 'true',
+      config: {}
     }
-  },
-  logs: {
-    //https://github.com/expressjs/morgan
-    morgan: {
-      enable: process.env.EXPRESSMODULE_MORGAN_ENABLE || 'true',
-      format: process.env.EXPRESSMODULE_MORGAN_FORMAT || 'short'
-    }
-  },
-  helmet: {
-    enable: process.env.EXPRESSMODULE_HELMET_ENABLE || 'true',
-    config: {}
-  }
+  };
 };
